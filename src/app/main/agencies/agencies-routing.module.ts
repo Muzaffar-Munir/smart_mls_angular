d2674011b0828dd348b4a5d211040577d0c98196
Muzaffar Munir <muzafarmunir@gmail.com>
2020-11-22 00:23:16 +0500

    agencies creating

--- src/app/main/agencies/agencies-routing.module.ts
+++ src/app/main/agencies/agencies-routing.module.ts
@@ -1,3 +1,4 @@
+import { AgenciesComponent } from './agencies.component';
 import { AgenciesCreateComponent } from './agencies-create/agencies-create.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,6 +7,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
+        component: AgenciesComponent
+    },
+    {
+        path: 'create',
         component: AgenciesCreateComponent
     }
 ];
--- src/app/main/agencies/agencies.module.ts
+++ src/app/main/agencies/agencies.module.ts
@@ -1,3 +1,6 @@
+import { AgenciesRoutingModule } from './agencies-routing.module';
+import { AgenciesCreateModule } from './agencies-create/agencies-create.module';
+import { FuseSharedModule } from './../../../@fuse/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AgenciesComponent } from './agencies.component';
@@ -7,7 +10,10 @@ import { AgenciesComponent } from './agencies.component';
 @NgModule({
   declarations: [AgenciesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FuseSharedModule,
+    AgenciesRoutingModule,
+    AgenciesCreateModule
   ]
 })
 export class AgenciesModule { }
